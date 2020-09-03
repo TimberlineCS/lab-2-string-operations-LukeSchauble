70dec77058652eabf6decc2ecae81d2ee10ee7f9
LukeSchauble <22luke.schauble@boiseschools.net>
2020-09-03 15:56:56 +0000

    Finished second challenge

--- StringOperations.java
+++ StringOperations.java
@@ -4,13 +4,22 @@
 public class StringOperations{
   public static void main(String args[]){
     String word = "chelsea";
-    int a = word.length();
-    int half = a/2;
+    int x = word.length();
+    int half = x/2;
     String firstHalf= word.substring(0,half);
     String secondHalf= word.substring(half);
     System.out.print(secondHalf);
-    System.out.print(firstHalf);
-    
+    System.out.println(firstHalf);
+    //Challenge 2
+    String a = "    ________\n";
+    String b = "   /_______/\\\n";
+    String c = "   \\ \\    / /\n";
+    String d = " ___\\ \\__/_/___\n";
+    String e = "/____\\ \\______/\\\n";
+    String f = "\\ \\   \\/ /   / /\n";
+    String g = " \\ \\  / /\\  / /\n";
+    String h = "  \\ \\/ /\\ \\/ /\n";
+    String i = "   \\_\\/  \\_\\/\n";
+    System.out.print(a+b+c+d+e+f+g+h+i);
   }
-
 }
